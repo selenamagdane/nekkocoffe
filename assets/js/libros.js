cddff5aab86ef9499225ef9e0ffce56c13760787
nivescho <danarodaschacon@gmail.com>
2024-04-12 17:56:01 -0500

    shekiavance

--- assets/js/libros.js
+++ assets/js/libros.js
@@ -1,52 +1,99 @@
+// Variable global para almacenar los títulos de los libros seleccionados
+let titulosLibros = '';
+
 document.addEventListener('DOMContentLoaded', function () {
-  // URL del archivo JSON
-  const url = '../../assets/JSON/libros.json';
+    // URL del archivo JSON
+    const url = './assets/JSON/libros.json';
 
-  // Obtener referencia al carrusel y su contenedor
-  const carouselInner = document.getElementById('carouselExampleIndicators');
+    // Obtener referencia al carrusel y su contenedor
+    const carouselInner = document.getElementById('carouselExampleIndicators');
 
-  // Función para cambiar las imágenes del carrusel según el género seleccionado
-  function cambiarImagenes(genero) {
+    
+    // Cargar el archivo JSON y cambiar las imágenes del carrusel al hacer clic en un botón de género
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            const botonesGenero = document.querySelectorAll('.genero');
+            botonesGenero.forEach(boton => {
+                boton.addEventListener('click', function () {
+                    const genero = data.libros[this.getAttribute('data-genero')];
+                    cambiarImagenes(genero);
+                });
+            });
+        })
+        .catch(error => console.error('Error al cargar el archivo JSON:', error));
+});
+
+// Función para cambiar las imágenes del carrusel según el género seleccionado
+function cambiarImagenes(genero) {
     const carouselItems = document.querySelectorAll('.carousel-item');
-    genero.forEach((libro, index) => {
-        const carouselItem = carouselItems[index];
+
+    // Eliminar los event listeners anteriores de los botones Agregar
+    carouselItems.forEach(carouselItem => {
+        const btnAgregar = carouselItem.querySelector('.btn');
+        btnAgregar.removeEventListener('click', mostrarTitulo);
+    });
+
+    // Asignar los libros y agregar los nuevos event listeners
+    carouselItems.forEach((carouselItem, index) => {
+        const libro = genero[index];
         const imagen = carouselItem.querySelector('img');
         imagen.src = libro.imagen;
+
+        // Agregar evento al botón Agregar de cada item del carrusel
+        const btnAgregar = carouselItem.querySelector('.btn');
+        btnAgregar.addEventListener('click', function() {
+            mostrarTitulo(libro);
+        });
     });
 }
 
-  // Cargar el archivo JSON y cambiar las imágenes del carrusel al hacer clic en un botón de género
-  fetch(url)
-      .then(response => response.json())
-      .then(data => {
-          const botonesGenero = document.querySelectorAll('.genero');
-          botonesGenero.forEach(boton => {
-              boton.addEventListener('click', function () {
-                  const genero = data.libros[this.getAttribute('data-genero')];
-                  cambiarImagenes(genero);
-              });
-          });
-      })
-      .catch(error => console.error('Error al cargar el archivo JSON:', error));
+function mostrarTitulo(libro) {
+    // Verificar si el título ya ha sido agregado antes de agregarlo
+    if (!titulosLibros.includes(libro.titulo)) {
+        // Agregar el título del libro al textarea y a la variable global
+        const textarea = document.getElementById('CntLibros');
+        titulosLibros += libro.titulo + '\n';
+        textarea.value = titulosLibros;
+
+        textarea.style.height = 'auto'; 
+        textarea.style.height = (textarea.scrollHeight + 2) + 'px';
+
+        const factura = document.getElementById("factura");
+        factura.style.height = `${factura.scrollHeight}px`;
+    }
+}
+
+//HORA
+
+// Obtiene la fecha y hora actual
+var fechaHoraActual = new Date();
+
+// Obtiene la parte de la hora en formato HH:MM
+var horaActual = fechaHoraActual.getHours() + ':' + fechaHoraActual.getMinutes();
+
+// Establece la hora actual en el campo de entrada
+document.getElementById('horaActual').value = horaActual;
+
+//DATOS DEL CLIENTE
+
+const guardarBtn = document.getElementById('guardarBtn');
+
+guardarBtn.addEventListener('click', (event) => {
+    event.preventDefault(); // Evita que el formulario se envíe automáticamente
+
+    const nombres = document.querySelector('.input-nombres').value;
+    const apellidos = document.querySelector('.input-apellidos').value;
+    const dni = document.querySelector('.input-dni').value;
+    const hora = document.querySelector('.input-hora').value;
+    const libros = titulosLibros.trim().split("\n");
+
+    // Verificar que los campos obligatorios estén llenos
+    if (nombres.trim() === '' || apellidos.trim() === '' || dni.trim() === '' || hora.trim() === '' || libros[0].trim() === '') {
+        alert('Por favor, llene todos los campos obligatorios.');
+        return; // Evita que el código siguiente se ejecute si hay campos vacíos
+    }
+
+    // Si todos los campos están llenos, redirige a la otra página con los datos como parámetros en la URL
+    window.location.href = `registro.html?nombres=${nombres}&apellidos=${apellidos}&dni=${dni}&hora=${hora}&libros=${libros}`;
 });
-
-
-
-    document.querySelector('button').addEventListener('click', function() {
-        const nombres = document.querySelector('input[type="text"]').value;
-        const apellidos = document.querySelector('input[type="text"][class="input"]').value;
-        const dni = document.querySelector('input[type="text"][class="input1"]').value;
-        const hora = document.querySelector('input[type="text"][class="input2"]').value;
-        
-        const cliente = {
-            nombres,
-            apellidos,
-            dni,
-            hora
-        };
-        
-        localStorage.setItem('cliente', JSON.stringify(cliente));
-        
-        window.location.href = 'registro.html';
-    });
-
--- assets/js/registro.js
+++ assets/js/registro.js
@@ -1,11 +1,22 @@
+//DATOS DEL CLIENTE
+window.onload = () => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const nombres = urlParams.get('nombres');
+    const apellidos = urlParams.get('apellidos');
+    const dni = urlParams.get('dni');
+    const hora = urlParams.get('hora');
+    const libros =urlParams.get('libros').split(',');
+    
+    document.querySelector('#nombres').innerHTML = nombres;
+    document.querySelector('#apellidos').innerHTML = apellidos;
+    document.querySelector('#dni').innerHTML = dni;
+    document.querySelector('#hora').innerHTML = hora;
 
-    window.onload = function() {
-        const cliente = JSON.parse(localStorage.getItem('cliente'));
-        
-        if (cliente) {
-            document.querySelector('input[type="text"]').value = cliente.nombres;
-            document.querySelector('input[type="text"][class="input"]').value = cliente.apellidos;
-            document.querySelector('input[type="text"][class="input1"]').value = cliente.dni;
-            document.querySelector('input[type="text"][class="input2"]').value = cliente.hora;
+    const $libros = document.querySelector("#libros");
+
+    for (const libro of libros) {
+        if (libro != "") {
+            $libros.innerHTML += `<span>${libro}</span><br />`;
         }
-    };
+    }
+};
